15eb6fbb7dbc267b5e4890a3e2dfd2bad9443cb6
Daniel Martin <drmartin@localhost.localdomain>
2020-05-19 20:34:07 -0500

    example instances with ssh key

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -1,6 +1,18 @@
 resource "aws_instance" "server1" {
-  ami           = "ami-0a54aef4ef3b5f881"
-  instance_type = "t2.micro"
-  count         = 1
-  subnet_id     = "${aws_subnet.subnet_main.id}"
+  ami                         = "ami-0a54aef4ef3b5f881"
+  instance_type               = "t2.micro"
+  count                       = 1
+  associate_public_ip_address = true
+  subnet_id                   = "${aws_subnet.subnet_main.id}"
+  vpc_security_group_ids = [
+    "${aws_security_group.sg_http.id}",
+    "${aws_security_group.sg_main.id}"
+  ]
+  key_name = "${aws_key_pair.key_class_file.key_name}"
+
+  tags = {
+    Name  = "server-1"
+    Owner = "terraform"
+    Env   = "dev"
+  }
 }
