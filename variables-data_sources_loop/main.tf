62b55aa355fbf8aa7a8f1f366161bf183c338c23
Daniel Martin <drmartin@localhost.localdomain>
2020-05-21 21:21:19 -0500

    implement data sources loop

--- variables-data_sources_loop/main.tf
+++ variables-data_sources_loop/main.tf
@@ -12,13 +12,44 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_subnet" "subnet1" {
-  vpc_id     = "${aws_vpc.main.id}"
-  cidr_block = "${var.subnet_cidr}"
+resource "aws_subnet" "subnets" {
+  #count                = "${length(var.azs_list)}"
+  count             = "${length(data.aws_availability_zones.azs.names)}"
+  vpc_id            = "${aws_vpc.main.id}"
+  availability_zone = "${element(data.aws_availability_zones.azs.names, count.index)}"
+  cidr_block        = "${element(var.subnet_cidr, count.index)}"
+
   tags = {
-    Name = "Subnet 1"
+    Name = "Subnet - ${count.index + 1}"
   }
 }
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.main.id}"  
+  tags = {
+    Name = "VPC Main GW"
+  }
+}
+
+resource "aws_route_table" "web_public_rt" {
+  vpc_id = "${aws_vpc.main.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+  tags = {
+    Name = "Public Subnet RT"
+  }
+}
+
+
+resource "aws_route_table_association" "public-subnet" {
+  count = "${length(var.subnet_cidr)}"
+  subnet_id = "${element(aws_subnet.subnets.*.id, count.index)}"
+  route_table_id = "${aws_route_table.web_public_rt.id}"
+}
+
+
+
 
 
 
--- variables-data_sources_loop/vars.tf
+++ variables-data_sources_loop/vars.tf
@@ -7,5 +7,13 @@ variable "vpc_cidr" {
 }
 
 variable "subnet_cidr" {
-  default = "192.168.1.0/24"
+  type    = "list"
+  default = ["192.168.1.0/24", "192.168.2.0/24", "192.168.3.0/24"]
 }
+
+variable "azs_list" {
+  type    = "list"
+  default = ["us-east-2a", "us-east-2b", "us-east-2c"]
+}
+
+data "aws_availability_zones" "azs" {}
